2422e513857e840fd9be957eed528ab530ac8074
Abdallah Gamal <body20002.test@gmail.com>
2022-06-09 20:44:36 +0200

    Modified ListOfFiles.java

--- src/main/java/com/anas/javautils/jls/output/ListOfFiles.java
+++ src/main/java/com/anas/javautils/jls/output/ListOfFiles.java
@@ -9,8 +9,11 @@ import java.io.IOException;
 public class ListOfFiles {
     public void printFiles() throws IOException {
         var argumentProcessor = ArgumentProcessor.getInstance();
-        FileInfo[] files = FilesHelper.getFiles(ArgumentProcessor.getInstance().hasOption(CLIOption.ALL),
-                ArgumentProcessor.getInstance().getTargetPath());
+
+        FileInfo[] files = FilesHelper.getFiles(
+            ArgumentProcessor.getInstance().hasOption(CLIOption.ALL),
+            ArgumentProcessor.getInstance().getTargetPath()
+        );
 
         if (argumentProcessor.hasOption(CLIOption.LONG)) {
             printLongFormat(files, argumentProcessor);
@@ -31,23 +34,39 @@ public class ListOfFiles {
     private void printLongFormat(final FileInfo[] files, final ArgumentProcessor argumentProcessor) {
         LongLength length = new LongLength();
 
+        boolean IS_NO_COLORS = ArgumentProcessor.getInstance().hasOption(CLIOption.NO_COLORS);
+        boolean IS_HUMAN_READABLE = argumentProcessor.hasOption(CLIOption.HUMAN_READABLE);
+
         for (FileInfo file : files) {
             length.setLongFileNameLength(
-                    (byte) Math.max(length.getLongFileNameLength(), file.getName(
-                            !ArgumentProcessor.getInstance().hasOption(CLIOption.NO_COLORS)).length()));
+                    (byte) Math.max(
+                        length.getLongFileNameLength(), file.getName(!IS_NO_COLORS).length()
+                )
+            );
+
             length.setLongDateLength(
-                    (byte) Math.max(length.getLongDateLength(), file.getCreationTime(
-                            !ArgumentProcessor.getInstance().hasOption(CLIOption.NO_COLORS)).length()));
+                    (byte) Math.max(
+                        length.getLongDateLength(), file.getCreationTime(!IS_NO_COLORS).length()
+                )
+            );
+
             length.setLongSizeLength(
-                    (byte) Math.max(length.getLongSizeLength(),
-                            file.getSize(argumentProcessor.hasOption(CLIOption.HUMAN_READABLE),
-                                    !ArgumentProcessor.getInstance().hasOption(CLIOption.NO_COLORS)).length()));
+                    (byte) Math.max(
+                        length.getLongSizeLength(), file.getSize(IS_HUMAN_READABLE, !IS_NO_COLORS).length()
+                )
+            );
+
             length.setLongOwnerLength(
-                    (byte) Math.max(length.getLongOwnerLength(), file.getOwner(
-                            !ArgumentProcessor.getInstance().hasOption(CLIOption.NO_COLORS)).length()));
+                    (byte) Math.max(
+                        length.getLongOwnerLength(), file.getOwner(!IS_NO_COLORS).length()
+                )
+            );
+
             length.setLongGroupLength(
-                    (byte) Math.max(length.getLongGroupLength(), file.getGroup(
-                            !ArgumentProcessor.getInstance().hasOption(CLIOption.NO_COLORS)).length()));
+                    (byte) Math.max(
+                    length.getLongGroupLength(), file.getGroup(!IS_NO_COLORS).length()
+                )
+            );
         }
 
         for (FileInfo file : files) {
@@ -55,35 +74,54 @@ public class ListOfFiles {
         }
     }
 
-    private String getLongFormat(final FileInfo fileInfo,
-                                 final ArgumentProcessor argumentProcessor, final LongLength length) {
-        var withColors = !argumentProcessor.hasOption(CLIOption.NO_COLORS);
-        String format = "%10s  " + "%" + -length.getLongSizeLength() + "s" +
-                ((fileInfo.isDirectory()? " " : "")  + " %" + (!argumentProcessor.hasOption(CLIOption.NO_OWNER) ?
-                        (-length.getLongOwnerLength() + "s  ") : "s")) +
-                "%" + (argumentProcessor.hasOption(CLIOption.GROUP) ? (-length.getLongGroupLength() + "s  ") : "s") +
-                "%" + (!argumentProcessor.hasOption(CLIOption.NO_DATE) ? (-length.getLongDateLength() + "s  ") : "s") +
-                "%s";
-        return String.format(format,
+    private String getLongFormat(
+        final FileInfo fileInfo,
+        final ArgumentProcessor argumentProcessor,
+        final LongLength length
+    ) {
+        var IS_WITH_NO_COLORS = !argumentProcessor.hasOption(CLIOption.NO_COLORS);
+        var IS_NO_OWNER = argumentProcessor.hasOption(CLIOption.NO_OWNER);
+        var IS_PRINT_GROUP = argumentProcessor.hasOption(CLIOption.GROUP);
+        var IS_NO_DATE = argumentProcessor.hasOption(CLIOption.NO_DATE);
+        var IS_HUMAN_READABLE = argumentProcessor.hasOption(CLIOption.HUMAN_READABLE);
 
+        String format = "%10s  " + "%" + -length.getLongSizeLength() + "s" +
+                ((fileInfo.isDirectory() ? " " : "") + " %" +
+                (!IS_NO_OWNER ? (-length.getLongOwnerLength() + "s  ") : "s")) + "%" +
+                (IS_PRINT_GROUP ? (-length.getLongGroupLength() + "s  ") : "s") + "%" +
+                (!IS_NO_DATE ? (-length.getLongDateLength() + "s  ") : "s") + "%s";
+
+        return String.format(format,
                 fileInfo.getPermissions(),
-                fileInfo.getSize(argumentProcessor.hasOption(CLIOption.HUMAN_READABLE), withColors),
-                (!argumentProcessor.hasOption(CLIOption.NO_OWNER) ? fileInfo.getOwner(withColors) : ""),
-                (argumentProcessor.hasOption(CLIOption.GROUP) ? fileInfo.getGroup(withColors) : ""),
-                (!argumentProcessor.hasOption(CLIOption.NO_DATE) ? fileInfo.getCreationTime(withColors) : ""),
-                getShortFormat(fileInfo, argumentProcessor, false));
+                fileInfo.getSize(IS_HUMAN_READABLE, IS_WITH_NO_COLORS),
+                !IS_NO_OWNER ? fileInfo.getOwner(IS_WITH_NO_COLORS) : "",
+                IS_PRINT_GROUP ? fileInfo.getGroup(IS_WITH_NO_COLORS) : "",
+                !IS_NO_DATE ? fileInfo.getCreationTime(IS_WITH_NO_COLORS) : "",
+                getShortFormat(fileInfo, argumentProcessor, false)
+        );
 
     }
 
-    private String getShortFormat(final FileInfo fileInfo,
-                                  final ArgumentProcessor argumentProcessor, final boolean withSize) {
-        var withColors = !argumentProcessor.hasOption(CLIOption.NO_COLORS);
-        return String.format((argumentProcessor.hasOption(CLIOption.NO_ICONS) ? "%s" : "%-2s ") + "%s"
-                + (fileInfo.isSymlink() ? " -> %s" : "%s") + (withSize ? "  %s" : "%s"),
-                (argumentProcessor.hasOption(CLIOption.NO_ICONS)? "" : fileInfo.getIcon(withColors)),
-                fileInfo.getName(withColors),
-                fileInfo.isSymlink() ? fileInfo.getSymlinkTarget(withColors) : "",
-                (withSize ? fileInfo.getSize(argumentProcessor.hasOption(CLIOption.HUMAN_READABLE), withColors) : ""));
+    private String getShortFormat(
+        final FileInfo fileInfo,
+        final ArgumentProcessor argumentProcessor,
+        final boolean withSize
+    ) {
+        var IS_WITH_NO_COLORS = !argumentProcessor.hasOption(CLIOption.NO_COLORS);
+        var IS_HUMAN_READABLE = argumentProcessor.hasOption(CLIOption.HUMAN_READABLE);
+        var IS_NO_ICONS = argumentProcessor.hasOption(CLIOption.NO_ICONS);
+
+        String format =
+            (IS_NO_ICONS ? "%s" : "%-2s ") + "%s" +
+            (fileInfo.isSymlink() ? " -> %s" : "%s") +
+            (withSize ? "  %s" : "%s");
+
+        return String.format(format,
+            IS_NO_ICONS ? "" : fileInfo.getIcon(IS_WITH_NO_COLORS),
+            fileInfo.getName(IS_WITH_NO_COLORS),
+            fileInfo.isSymlink() ? fileInfo.getSymlinkTarget(IS_WITH_NO_COLORS) : "",
+            withSize ? fileInfo.getSize(IS_HUMAN_READABLE, IS_WITH_NO_COLORS) : ""
+        );
 
     }
 }
