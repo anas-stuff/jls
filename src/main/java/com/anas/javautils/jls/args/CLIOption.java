2b609a0505310f2ad94e3ad9f25e371d5e4eb553
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-09 08:17:20 +0200

    Improve options 😉

--- src/main/java/com/anas/javautils/jls/args/CLIOption.java
+++ src/main/java/com/anas/javautils/jls/args/CLIOption.java
@@ -3,19 +3,19 @@ package com.anas.javautils.jls.args;
 import org.apache.commons.cli.Option;
 
 public enum CLIOption {
-    HELP("h", "help", false, "Show this help"),
+    HELP("", "help", false, "Show this help"),
     VERSION("v", "version", false, "Show version"),
     ALL("a", "all", false, "Print all files including hidden files"),
     LONG("l", "long", false, "Print the long format"),
-    GROUP("g", "group", false, "Print the group"),
+    GROUP("G", "group", false, "Print the group"),
     HUMAN_READABLE("h", "human-readable", false, "Print the human readable format"),
     SIZE("s", "size", false, "Print the size"),
     SORT_BY_SIZE("S", "", false, "Sort by size (largest first)"),
     RECURSIVE("R", "recursive", false, "List subdirectories recursively"),
     NO_ICONS("nc", "no-icons", false, "Don't show icons"),
-    ICON_COLOR("ic", "icon-color", true, "Set the icon color (like #ff0000), default is auto"),
+    ICON_COLOR("ic", "icons-color", true, "Set the icon color (like #ff0000), default is auto"),
     TEXT_COLOR("tc", "text-color", true, "Set the text color (like #ff0000), default is #ffffff"),
-    NO_OWNER("no", "no-owner", false, "Don't show owner in long format"),
+    NO_OWNER("", "no-owner", false, "Don't show owner in long format"),
     NO_COLORS("", "no-colors", false, "Don't use colors"),
     NO_DATE("nd", "no-date", false, "Don't show date in long format"),
     NO_NAME("nn", "no-name", false, "Don't show name"),
