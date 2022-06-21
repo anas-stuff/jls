ed36548211da62ac2f6cd2924b4b3e0d2347a1e4
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-06-21 12:43:17 +0200

    Finally, add the java docs to the Main class ☺️
    tttttoooyytuggjgjfjfynnttttthfhfhfhfhfhhghghhgghgghhghghgghghg - just
    habed(هبد) on  the keyboard

--- src/main/java/com/anas/javautils/jls/Main.java
+++ src/main/java/com/anas/javautils/jls/Main.java
@@ -8,12 +8,19 @@ import java.io.IOException;
 public class Main {
     public static final String version = "0.3.2";
 
+    /**
+     * The entry point :D
+     * It creates an instance of the ArgumentProcessor class, which parses the command line arguments and stores them in a
+     * map
+     *
+     * @param args The arguments passed to the program.
+     */
     public static void main(final String[] args) {
         ArgumentProcessor.getInstance().process(args);
         try {
             new ListOfFiles().printFiles();
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace(); // TODO: 6/21/22 Use a logger and the massage function.
         }
     }
 }
